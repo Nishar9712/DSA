a26963cb68f9f3e85430b0deb764b11aff7db6fb
Nishar Dayma <daymanishar123@gmail.com>
2026-07-03 15:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (11.90%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+
+        if (n<=0) return false;
+
+        return ( (n & (n-1)) == 0);
+        /*
         bool ans = false;
         int a = 1;
-        
+
         for(int i=0; i<=30; i++){
             
             if (a == n) ans = true;
@@ -11,7 +16,8 @@ public:
             if (a < INT_MAX/2) 
                 a *=2;
         }
-
         return ans;
+        */
+
     }
 };
